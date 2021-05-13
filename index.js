3aabaa1bafe6545ac930ce8c09b2b539bcd0eade
StaticTolyan <savchenkotolyan@gmail.com>
2021-05-13 13:40:25 +0300

    formula

--- index.js
+++ index.js
@@ -160,135 +160,7 @@ $(document).ready(function() {
     .appendTo( $('#finalbtns') );
 
     var prev = $('#prev').DataTable( {
-        language: {
-            "lengthMenu": "Показати _MENU_ записів",
-            "infoFiltered": "(відфільтровано з _MAX_ записів)",
-            "search": "Пошук:",
-            "paginate": {
-                "first": "<<",
-                "previous": "<",
-                "next": ">",
-                "last": ">>"
-            },
-            "aria": {
-                "sortAscending": ": активуйте, щоб сортувати колонку за зростанням",
-                "sortDescending": ": активуйте, щоб сортувати колонку за спаданням"
-            },
-            "autoFill": {
-                "cancel": "Відміна",
-                "fill": "Заповнити всі клітинки з <i>%d<\/i>",
-                "fillHorizontal": "Заповнити клітинки горизонтально",
-                "fillVertical": "Заповнити клітинки вертикально"
-            },
-            "buttons": {
-                "collection": "Список <span class=\"ui-button-icon-primary ui-icon ui-icon-triangle-1-s\"><\/span>",
-                "colvis": "Видимість колонок",
-                "colvisRestore": "Відновити видимість",
-                "copy": "Копіювати",
-                "copyKeys": "Нажміть ctrl або u2318 + C щоб копіювати інформацію з таблиці до вашого буферу обміну.<br \/><br \/>Щоб відмінити нажміть на це повідомлення або Esc",
-                "copySuccess": {
-                    "1": "Скопійовано 1 рядок в буфер обміну",
-                    "_": "Скопійовано %d рядків в буфер обміну"
-                },
-                "copyTitle": "Копіювати в буфер обміну",
-                "csv": "CSV",
-                "excel": "Excel",
-                "pageLength": {
-                    "-1": "Показати усі рядки",
-                    "1": "Показати 1 рядок",
-                    "_": "Показати %d рядки"
-                },
-                "pdf": "PDF",
-                "print": "Друкувати"
-            },
-            "emptyTable": "Ця таблиця не містить даних",
-            "info": "Показано від _START_ по _END_ з _TOTAL_ записів",
-            "infoEmpty": "Показано від 0 по 0 з 0 записів",
-            "infoThousands": ",",
-            "loadingRecords": "Завантаження",
-            "processing": "Опрацювання...",
-            "searchBuilder": {
-                "add": "Додати умову",
-                "button": {
-                    "0": "Розширений пошук",
-                    "_": "Розширений пошук (%d)"
-                },
-                "clearAll": "Очистити все",
-                "condition": "Умова",
-                "conditions": {
-                    "date": {
-                        "after": "Після",
-                        "before": "До",
-                        "between": "Між",
-                        "empty": "Пусто",
-                        "equals": "Дорівнює",
-                        "not": "Не",
-                        "notBetween": "Не між",
-                        "notEmpty": "Не пусто"
-                    },
-                    "number": {
-                        "between": "Між",
-                        "empty": "Пусто",
-                        "equals": "Дорівнює",
-                        "gt": "Більше ніж",
-                        "gte": "Більше або дорівнює",
-                        "lt": "Менше ніж",
-                        "lte": "Менше або дорівнює",
-                        "not": "Не",
-                        "notBetween": "Не між",
-                        "notEmpty": "Не пусто"
-                    },
-                    "string": {
-                        "contains": "Містить",
-                        "empty": "Пусто",
-                        "endsWith": "Закінчується на",
-                        "equals": "Дорівнює",
-                        "not": "Не",
-                        "notEmpty": "Не пусто",
-                        "startsWith": "Починається з"
-                    }
-                },
-                "data": "Параметр",
-                "deleteTitle": "Видалити правило фільтрування",
-                "leftTitle": "Відступні критерії",
-                "logicAnd": "I",
-                "logicOr": "Або",
-                "rightTitle": "Відступні критерії",
-                "title": {
-                    "0": "Розширений пошук",
-                    "_": "Розширений пошук (%d)"
-                },
-                "value": "Значення"
-            },
-            "searchPanes": {
-                "clearMessage": "Очистити все",
-                "collapse": {
-                    "0": "Пошукові Панелі",
-                    "_": "Пошукові Панелі (%d)"
-                },
-                "count": "{total}",
-                "countFiltered": "{shown} ({total})",
-                "emptyPanes": "Немає Пошукових Панелей",
-                "loadMessage": "Завантаження Пошукових Панелей",
-                "title": "Активній фільтри - %d"
-            },
-            "select": {
-                "rows":{
-                    "1": "%d рядок вибрано",
-                    "_": "%d рядків вибрано"
-                },
-                "cells": {
-                    "1": "1 клітинку вибрано",
-                    "_": "%d клітинок вибрано"
-                },
-                "columns": {
-                    "1": "1 колонку вибрано",
-                    "_": "%d колонок вибрано"
-                }
-            },
-            "thousands": ",",
-            "zeroRecords": "Не знайдено жодних записів"
-        },
+        language: lang,
         responsive: true,
         fixedHeader: true,
         fixedColumns: true,
@@ -319,7 +191,6 @@ $(document).ready(function() {
 
     var finalData = final.rows().data();
     var prevData = prev.rows().data();
- 
 
     var vyshi = final.column(3).data().unique();
 
@@ -361,18 +232,18 @@ $(document).ready(function() {
         top[j][0]=vyshi[j];
         top[j][1]=k;
         top[j][2]=za;
-        top[j][3]=Math.round((za/k)*100) + '%';
+        top[j][3]=Math.round((za/k)*10000)/100 + '%';
         top[j][4]=a;
-        top[j][5]=Math.round((a/k)*100) + '%';
+        top[j][5]=Math.round((a/k)*10000)/100 + '%';
         top[j][6]=ya;
-        top[j][7]=Math.round((ya/k)*100) + '%';
+        top[j][7]=Math.round((ya/k)*10000)/100 + '%';
         top[j][8]=v;
-        top[j][9]=Math.round((v/k)*100) + '%';
+        top[j][9]=Math.round((v/k)*10000)/100 + '%';
         top[j][10]=Ga;
-        top[j][11]=Math.round((Ga/k)*100) + '%';
+        top[j][11]=Math.round((Ga/k)*10000)/100 + '%';
         top[j][12]=Ba;
-        top[j][13]=Math.round((Ba/k)*100) + '%';
-        top[j][14]=a+za*5+ya*(-3)+v*(-10);
+        top[j][13]=Math.round((Ba/k)*10000)/100 + '%';
+        top[j][14]=Math.round(((a+za*5+ya*(-3)+v*(-10))/k)*100)/100;
     }
     
 
