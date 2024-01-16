fd0eff3ba5b9f5d965f4df984d5db9b7d3e9c297
StaticTolyan <83454652+StaticTolyan@users.noreply.github.com>
2024-01-16 15:54:34 +0200

    Magistr

--- index.js
+++ index.js
@@ -1,299 +1,369 @@
-var lang = {
-    "lengthMenu": "Показати _MENU_ записів",
-    "infoFiltered": "(відфільтровано з _MAX_ записів)",
-    "search": "Пошук:",
-    "paginate": {
-        "first": "<<",
-        "previous": "<",
-        "next": ">",
-        "last": ">>"
-    },
-    "aria": {
-        "sortAscending": ": активуйте, щоб сортувати колонку за зростанням",
-        "sortDescending": ": активуйте, щоб сортувати колонку за спаданням"
-    },
-    "autoFill": {
-        "cancel": "Відміна",
-        "fill": "Заповнити всі клітинки з <i>%d<\/i>",
-        "fillHorizontal": "Заповнити клітинки горизонтально",
-        "fillVertical": "Заповнити клітинки вертикально"
-    },
-    "buttons": {
-        "collection": "Список <span class=\"ui-button-icon-primary ui-icon ui-icon-triangle-1-s\"><\/span>",
-        "colvis": "Видимість колонок",
-        "colvisRestore": "Відновити видимість",
-        "copy": "Копіювати",
-        "copyKeys": "Нажміть ctrl або u2318 + C щоб копіювати інформацію з таблиці до вашого буферу обміну.<br \/><br \/>Щоб відмінити нажміть на це повідомлення або Esc",
-        "copySuccess": {
-            "1": "Скопійовано 1 рядок в буфер обміну",
-            "_": "Скопійовано %d рядків в буфер обміну"
-        },
-        "copyTitle": "Копіювати в буфер обміну",
-        "csv": "CSV",
-        "excel": "Excel",
-        "pageLength": {
-            "-1": "Показати усі рядки",
-            "1": "Показати 1 рядок",
-            "_": "Показати %d рядки"
-        },
-        "pdf": "PDF",
-        "print": "Друкувати"
-    },
-    "emptyTable": "Ця таблиця не містить даних",
-    "info": "Показано від _START_ по _END_ з _TOTAL_ записів",
-    "infoEmpty": "Показано від 0 по 0 з 0 записів",
-    "infoThousands": ",",
-    "loadingRecords": "Завантаження",
-    "processing": "Опрацювання...",
-    "searchBuilder": {
-        "add": "Додати умову",
-        "button": {
-            "0": "Розширений пошук",
-            "_": "Розширений пошук (%d)"
-        },
-        "clearAll": "Очистити все",
-        "condition": "Умова",
-        "conditions": {
-            "date": {
-                "after": "Після",
-                "before": "До",
-                "between": "Між",
-                "empty": "Пусто",
-                "equals": "Дорівнює",
-                "not": "Не",
-                "notBetween": "Не між",
-                "notEmpty": "Не пусто"
-            },
-            "number": {
-                "between": "Між",
-                "empty": "Пусто",
-                "equals": "Дорівнює",
-                "gt": "Більше ніж",
-                "gte": "Більше або дорівнює",
-                "lt": "Менше ніж",
-                "lte": "Менше або дорівнює",
-                "not": "Не",
-                "notBetween": "Не між",
-                "notEmpty": "Не пусто"
-            },
-            "string": {
-                "contains": "Містить",
-                "empty": "Пусто",
-                "endsWith": "Закінчується на",
-                "equals": "Дорівнює",
-                "not": "Не",
-                "notEmpty": "Не пусто",
-                "startsWith": "Починається з"
-            }
-        },
-        "data": "Параметр",
-        "deleteTitle": "Видалити правило фільтрування",
-        "leftTitle": "Відступні критерії",
-        "logicAnd": "I",
-        "logicOr": "Або",
-        "rightTitle": "Відступні критерії",
-        "title": {
-            "0": "Розширений пошук",
-            "_": "Розширений пошук (%d)"
-        },
-        "value": "Значення"
-    },
-    "searchPanes": {
-        "clearMessage": "Очистити все",
-        "collapse": {
-            "0": "Пошукові Панелі",
-            "_": "Пошукові Панелі (%d)"
-        },
-        "count": "{total}",
-        "countFiltered": "{shown} ({total})",
-        "emptyPanes": "Немає Пошукових Панелей",
-        "loadMessage": "Завантаження Пошукових Панелей",
-        "title": "Активній фільтри - %d"
-    },
-    "select": {
-        "rows":{
-            "1": "%d рядок вибрано",
-            "_": "%d рядків вибрано"
-        },
-        "cells": {
-            "1": "1 клітинку вибрано",
-            "_": "%d клітинок вибрано"
-        },
-        "columns": {
-            "1": "1 колонку вибрано",
-            "_": "%d колонок вибрано"
+// Ініціалізація карти
+var map = L.map('map').setView([49.2324, 28.4736], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© Учасники OpenStreetMap'
+}).addTo(map);
+
+var map2 = L.map('map2').setView([49.2324, 28.4736], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© Учасники OpenStreetMap'
+}).addTo(map2);
+
+//L.marker([49.2324, 28.4736]).addTo(map).bindPopup("Center Vin");
+//-----------------------------------------------------------------------------------------------
+
+function colorMarker(color) {
+  const svgTemplate = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker">
+        <path fill-opacity=".25" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/>
+        <path stroke="#fff" fill="${color}" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/>
+      </svg>`;
+
+  const icon = L.divIcon({
+    className: "marker",
+    html: svgTemplate,
+    iconSize: [40, 40],
+    iconAnchor: [12, 24],
+    popupAnchor: [7, -16],
+  });
+
+  return icon;
+}
+
+function addIconMarker(coord, icon) {
+  let markers = L.markerClusterGroup();
+
+  const marker = L.marker(coord, {
+    icon: icon,
+  });
+  markers.addLayer(marker);
+  
+  map2.addLayer(markers);
+  return markers;
+}
+
+function addColorMarkers(coords, color) {
+  let markers = L.markerClusterGroup();
+
+  coords.forEach(coord => {
+    const marker = L.marker(coord, {
+      icon: colorMarker(color),
+    });
+    markers.addLayer(marker);
+  });
+
+  map.addLayer(markers);
+  return markers;
+}
+
+// Функція для відображення маркерів
+function addMarkers(coords) {
+  let markers = L.markerClusterGroup();
+
+  coords.forEach(coord => {
+    const marker = L.marker(coord);
+    markers.addLayer(marker);
+  });
+
+  map.addLayer(markers);
+  return markers;
+}
+
+//-----------------------------------------------------------------------------------------------
+
+function planRoute(coordinates, start, end) {
+
+  // Оптимізувати маршрут 3-OPT алгоритмом
+  coordinates = optimizeRoute3OPT(coordinates);
+
+  // Створити маршрут з оптимізованими координатами
+  var route = L.Routing.control({
+    waypoints: [
+      L.latLng(start),
+      ...coordinates.map(c => L.latLng(c)),
+      L.latLng(end)
+    ],
+    lineOptions: {
+      styles: [{ color: "green", opacity: 0.7, weight: 5 }],
+    }
+  }).addTo(map2);
+
+  return route;
+}
+
+// Функція оптимізації 3-OPT 
+function optimizeRoute3OPT(coordinates) {
+
+  let optimizedRoute = [...coordinates];
+
+  while (true) {
+
+    // Знайти найкращу можливу 3-OPT заміну
+    let bestGain = 0;
+    let bestIndexes = [];
+
+    for (let i = 0; i < optimizedRoute.length - 2; i++) {
+      for (let j = i + 1; j < optimizedRoute.length - 1; j++) {
+        for (let k = j + 1; k < optimizedRoute.length; k++) {
+
+          // Обчислити виграш від заміни
+          let gain = calcGain(optimizedRoute, i, j, k);
+
+          if (gain > bestGain) {
+            bestGain = gain;
+            bestIndexes = [i, j, k];
+          }
         }
-    },
-    "thousands": ",",
-    "zeroRecords": "Не знайдено жодних записів"
-};
-
-$(document).ready(function() {
-    var final = $('#final').DataTable( {
-        language: lang,
-        responsive: true,
-        fixedHeader: true,
-        fixedColumns: true,
-        buttons: [
-            'colvis',
-            {
-                extend: 'collection',
-                text: 'Експорт',
-                buttons: [ 'excel', 
-                    {
-                    extend: 'pdfHtml5',
-                    exportOptions: {
-                        modifier: {
-                            page: 'current'
-                        }
-                    },
-                    orientation: 'landscape',
-                    title: ''
-                    }
-                    , 'print']
-            }            
-        ],
-        dom: 'Qlfrtip',
-        lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, "Всі"] ]
-    } );
-    final.buttons().container()
-    .appendTo( $('#finalbtns') );
-
-    var prev = $('#prev').DataTable( {
-        language: lang,
-        responsive: true,
-        fixedHeader: true,
-        fixedColumns: true,
-        buttons: [
-            'colvis',
-            {
-                extend: 'collection',
-                text: 'Експорт',
-                buttons: [ 'excel', 
-                    {
-                    extend: 'pdfHtml5',
-                    exportOptions: {
-                        modifier: {
-                            page: 'current'
-                        }
-                    },
-                    orientation: 'landscape',
-                    title: ''
-                    }
-                    , 'print']
-            }            
-        ],
-        dom: 'Qlfrtip',
-        lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, "Всі"] ]
-    } );
-    prev.buttons().container()
-    .appendTo( $('#prevbtns') );
-
-    var finalData = final.rows().data();
-    var prevData = prev.rows().data();
-
-    var vyshi = final.column(3).data().unique();
-
-    var top = new Array(vyshi.length);
-    for (var i = 0; i < top.length; i++) {
-        top[i] = new Array(15).fill(0);
+      }
     }
 
-    for (let j = 0; j < vyshi.length; j++) {
-        k=0;
-        za=0;
-        a=0;
-        ya=0;
-        v=0;
-        Ga=0;
-        Ba=0;
-        bal=0;
-        for (let i = 0; i < finalData.length; i++) {
-            if(finalData[i][3] == vyshi[j]){
-                if(finalData[i][8]== "Зразкова акредитація"){
-                    za++;
-                    Ga++;
-                }
-                if(finalData[i][8]== "Акредитація"){
-                    a++;
-                    Ga++;
-                }
-                if(finalData[i][8]== "Умовна (відкладена) акредитація"){
-                    ya++;
-                    Ba++;
-                }
-                if(finalData[i][8]== "Відмова в акредитації"){
-                    v++;
-                    Ba++;
-                }
-                k++;
-            } 
-        }
-        top[j][0]=vyshi[j];
-        top[j][1]=k;
-        top[j][2]=za;
-        top[j][3]=Math.round((za/k)*10000)/100 + '%';
-        top[j][4]=a;
-        top[j][5]=Math.round((a/k)*10000)/100 + '%';
-        top[j][6]=ya;
-        top[j][7]=Math.round((ya/k)*10000)/100 + '%';
-        top[j][8]=v;
-        top[j][9]=Math.round((v/k)*10000)/100 + '%';
-        top[j][10]=Ga;
-        top[j][11]=Math.round((Ga/k)*10000)/100 + '%';
-        top[j][12]=Ba;
-        top[j][13]=Math.round((Ba/k)*10000)/100 + '%';
-        top[j][14]=Math.round(((a+za*5+ya*(-3)+v*(-10))/k)*100)/100;
+    // Якщо не знайдено покращення, зупинитися
+    if (bestGain <= 0) {
+      break;
     }
-    
 
-    
-    
-    var TOPtable = $('#Top').DataTable( {
-        language: lang,
-        responsive: true,
-        fixedHeader: true,
-        fixedColumns: true,
-        buttons: [
-            'colvis',
-            {
-                extend: 'collection',
-                text: 'Експорт',
-                buttons: [ 'excel', 
-                    {
-                    extend: 'pdfHtml5',
-                    exportOptions: {
-                        modifier: {
-                            page: 'current'
-                        }
-                    },
-                    orientation: 'landscape',
-                    title: ''
-                    }
-                    , 'print']
-            }            
-        ],
-        dom: 'Qlfrtip',
-        lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, "Всі"] ],
-        data: top,
-        columns: [
-            { title: 'Назва Університету' },
-            { title: 'Справ' },
-            { title: 'Зразкових'},
-            { title: '% Зразкових'},
-            { title: 'Акредитацій'},
-            { title: '% Акредитацій'},
-            { title: 'Умовних'},
-            { title: '% Умовних'},
-            { title: 'Відмов'},
-            { title: '% Відмов'},
-            { title: 'Успішних'},
-            { title: '% Успішних'},
-            { title: 'Неуспішних'},
-            { title: '% Неуспішних'},
-            { title: 'БАЛ' }
-        ]
-    } );
-    TOPtable.buttons().container()
-    .appendTo( $('#topbtns') );
-} );
+    // Зробити кращу заміну
+    make3OPTSwap(optimizedRoute, bestIndexes[0], bestIndexes[1], bestIndexes[2]);
+  }
+
+  return optimizedRoute;
+
+}
+
+// Функція обчислення виграшу від 3-OPT заміни
+function calcGain(route, i, j, k) {
+
+  // Відстань до заміни
+  let d0 = calcDistance(route[i - 1], route[i]) +
+    calcDistance(route[j - 1], route[j]) +
+    calcDistance(route[k - 1], route[k]);
+
+  // Відстань після заміни
+  let d1 = calcDistance(route[i - 1], route[j - 1]) +
+    calcDistance(route[j], route[k]) +
+    calcDistance(route[k], route[i]);
+
+  // Різниця відстаней
+  let gain = d0 - d1;
+
+  return gain;
+
+}
+
+// Функція обчислення відстані між двома точками
+function calcDistance(a, b) {
+  // Код обчислення відстані між a і b
+}
+
+
+// Функція заміни 3 ребер
+function make3OPTSwap(route, i, j, k) {
+
+  // Зберегти частину маршруту для реверсу
+  let reversedPart = route.slice(j, k + 1);
+
+  // Реверснути частину маршруту
+  reversedPart.reverse();
+
+  // Скласти новий маршрут
+  let newRoute = [
+    ...route.slice(0, i + 1),
+    ...reversedPart,
+    ...route.slice(k + 1)
+  ];
+
+  return newRoute;
+
+}
+
+
+//-----------------------------------------------------------------------------------------------
+
+function kMeans(coords, k) {
+  let centroids = coords.slice(0);
+  centroids = centroids.sort(() => 0.5 - Math.random());
+  centroids = centroids.slice(0, k);
+
+  let oldCentroids;
+  let iterations = 0;
+  let maxIterations = 100;
+  let clusters = new Array(k);
+
+  do {
+    // Перед кожною ітерацією ініціалізуємо масив кластерів
+    for (let i = 0; i < k; i++) clusters[i] = [];
+
+    for (let coord of coords) {
+      let distances = centroids.map(centroid => euclideanDistance(coord, centroid));
+      let closestCentroidIndex = distances.indexOf(Math.min(...distances));
+      clusters[closestCentroidIndex].push(coord);
+    }
+
+    oldCentroids = centroids.slice();
+
+    centroids = clusters.map(cluster => {
+      if (cluster.length === 0) return null; // Обробка порожнього кластера
+      let sums = cluster.reduce((acc, coord) => [acc[0] + coord[0], acc[1] + coord[1]], [0, 0]);
+      return sums.map(sum => sum / cluster.length);
+    });
+
+    // Видалення центроїдів, які не мають призначених точок
+    centroids = centroids.filter(centroid => centroid != null);
+    clusters = clusters.filter(cluster => cluster.length > 0);
+
+    iterations++;
+  } while (!centroidsEqual(oldCentroids, centroids) && iterations < maxIterations);
+
+  return clusters;
+}
+
+function euclideanDistance(coord1, coord2) {
+  return Math.sqrt(
+    (coord1[0] - coord2[0]) ** 2 +
+    (coord1[1] - coord2[1]) ** 2
+  );
+}
+
+function centroidsEqual(centroids1, centroids2) {
+  if (centroids1.length !== centroids2.length) return false;
+  return centroids1.every((centroid, i) => {
+    return (
+      centroid[0] === centroids2[i][0] &&
+      centroid[1] === centroids2[i][1]
+    );
+  });
+}
+
+
+
+
+// Читання даних з файлу
+const loadButton = document.getElementById('loadButton');
+const klusterButton = document.getElementById('klusterButton');
+const routingButton = document.getElementById('routingButton');
+const fileInput = document.getElementById('fileInput');
+var fromFileLayer;
+var clustersfromFile;
+var clustersLayers;
+var coordinatesArray;
+var map2legend;
+
+
+
+//-----------------------------------------------------------------------------------------------
+
+loadButton.addEventListener('click', () => {
+  const file = fileInput.files[0];
+
+  const reader = new FileReader();
+  reader.onload = () => {
+    const data = reader.result;
+    const rows = data.split('\r\n');
+
+    coordinatesArray = rows.map(row => {
+      const [x, y] = row.split(',').map(Number);
+      return [x, y];
+    });
+
+    console.log(coordinatesArray);
+    fromFileLayer = addMarkers(coordinatesArray);
+
+
+
+  }
+
+  reader.readAsText(file);
+});
+
+//-----------------------------------------------------------------------------------------------
+
+klusterButton.addEventListener('click', () => {
+  const k = document.getElementById('kBus').value; // Максимальна кількість точок в одному кластері
+  const clusters = kMeans(coordinatesArray, k);
+  clustersfromFile = clusters;
+  console.log(clusters);
+
+  map.removeLayer(fromFileLayer);
+  clustersfromFile.forEach(cluster => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    addColorMarkers(cluster, "#" + randomColor);
+  });
+
+  $('#clusterSelect').removeAttr('disabled');
+  $('#clusterSelect').attr({
+    "max": clusters.length
+  });
+
+});
+
+routingButton.addEventListener('click', async () => {
+
+  map2.eachLayer(layer => {
+    map2.removeLayer(layer);
+  });
+
+  if(map2legend)
+  map2legend.remove();
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© Учасники OpenStreetMap'
+  }).addTo(map2);
+
+  const [schoolCoords, depoCoords] = await Promise.all([
+    getCoordinates(document.getElementById('school').value),
+    getCoordinates(document.getElementById('depo').value)
+  ]);
+
+  const busNum = document.getElementById('clusterSelect').value - 1;
+
+  // Значки для маркерів
+  var busIcon = L.icon({
+    iconUrl: 'img/bus-icon.png',
+    iconSize: [64, 64],
+    iconAnchor: [32, 64],
+    popupAnchor: [0, -64]
+  });
+
+  var personIcon = L.icon({
+    iconUrl: 'img/person-icon.png',
+    iconSize: [64, 64],
+    iconAnchor: [32, 64],
+    popupAnchor: [0, -64]
+  });
+
+  var schoolIcon = L.icon({
+    iconUrl: 'img/school-icon.png',
+    iconSize: [64, 64],
+    iconAnchor: [32, 64],
+    popupAnchor: [0, -64]
+  });
+
+  map2legend = planRoute(clustersfromFile[busNum], schoolCoords, depoCoords);
+
+  addIconMarker(schoolCoords, schoolIcon);
+  addIconMarker(depoCoords, busIcon);
+
+});
+
+function getCoordinates(address) {
+  return new Promise((resolve, reject) => {
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.length > 0) {
+          resolve([Number(data[0].lat), Number(data[0].lon)]);
+        } else {
+          reject('Адресу не знайдено');
+        }
+      })
+      .catch(error => {
+        reject('Помилка під час запиту');
+      });
+  });
+}
+
+//-----------------------------------------------------------------------------------------------
